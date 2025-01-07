3731bdc94678e20c4e66d8480c17da13854375a2
John Flavin <flavinj@gmail.com>
2025-01-07 12:28:41 -0600

    Add autogenerated spring boot app

---
+++ orchestration/temporal-java/src/main/java/edu/washu/tag/temporaljava/TemporalJavaApplication.java
@@ -0,0 +1,13 @@
+package edu.washu.tag.temporaljava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemporalJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemporalJavaApplication.class, args);
+	}
+
+}
