7a0000dcdeb3e89f62c8aef83f13e670b78f8dac
John Flavin <flavinj@gmail.com>
2025-01-10 10:32:06 -0600

    Change task queue and ingest activity inputs to fit with in-progress python worker

--- orchestration/temporal-java/src/main/java/edu/washu/tag/temporal/model/IngestHl7FilesToDeltaLakeInput.java
+++ orchestration/temporal-java/src/main/java/edu/washu/tag/temporal/model/IngestHl7FilesToDeltaLakeInput.java
@@ -2,4 +2,4 @@ package edu.washu.tag.temporal.model;
 
 import java.util.List;
 
-public record IngestHl7FilesToDeltaLakeInput(String outputRootPath, List<String> hl7RelativePaths) { }
+public record IngestHl7FilesToDeltaLakeInput(String deltaTable, List<String> hl7Files) { }
