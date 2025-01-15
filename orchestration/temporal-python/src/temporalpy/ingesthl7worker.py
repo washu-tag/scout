63320f2bf9bcffa9c1028e4dcf422c8cf1ee8246
John Flavin <flavinj@gmail.com>
2025-01-15 09:28:49 -0600

    Fix python app startup issues

--- orchestration/temporal-python/src/temporalpy/ingesthl7worker.py
+++ orchestration/temporal-python/src/temporalpy/ingesthl7worker.py
@@ -1,19 +1,31 @@
+import argparse
 import asyncio
+import concurrent.futures
+import logging
+import os
+import sys
 
 from temporalio.client import Client
 from temporalio.worker import Worker
 
-from temporalpy.activities.ingesthl7 import TASK_QUEUE_NAME, ingest_hl7_files_to_delta_lake
+from temporalpy.activities.ingesthl7 import TASK_QUEUE_NAME, ingest_hl7_files_to_delta_lake_activity
+
+log = logging.getLogger("workflow_worker")
 
 
 async def run_worker(temporal_address: str, namespace: str) -> None:
-    client = await Client.connect(args.temporal_address, namespace=args.namespace)
-    worker = Worker(
-        client,
-        task_queue=TASK_QUEUE_NAME,
-        activities=[ingest_hl7_files_to_delta_lake],
-    )
+    client = await Client.connect(temporal_address, namespace=namespace)
+    with concurrent.futures.ThreadPoolExecutor() as pool:
+        worker = Worker(
+            client,
+            task_queue=TASK_QUEUE_NAME,
+            activities=[ingest_hl7_files_to_delta_lake_activity],
+            activity_executor = pool,
+        )
+
+    log.info("Starting worker...")
     await worker.run()
+    log.info("Worker stopped")
 
 
 async def main(argv=None):
@@ -29,24 +41,17 @@ async def main(argv=None):
         help="Turn on debug logging",
         action="store_true",
     )
-    parser.add_argument(
-        "temporal_address",
-        help="Temporal service address",
-        default="temporal-frontend.temporal:7233",
-    )
-    parser.add_argument(
-        "namespace",
-        help="Temporal namespace",
-        default="default",
-    )
     args = parser.parse_args(argv)
+
+    temporal_address = os.environ.get("TEMPORAL_ADDRESS", "temporal-frontend.temporal:7233")
+    temporal_namespace = os.environ.get("TEMPORAL_NAMESPACE", "default")
 
     logging.basicConfig(
         level=logging.DEBUG if args.debug else logging.INFO,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     )
 
-    await run_worker(args.temporal_address, args.namespace)
+    await run_worker(temporal_address, temporal_namespace)
 
 
 if __name__ == "__main__":
