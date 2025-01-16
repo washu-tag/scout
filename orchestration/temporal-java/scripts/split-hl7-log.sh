39ba053e1f9748b816061090f6e66626c6a9db6f
John Flavin <flavinj@gmail.com>
2025-01-16 15:53:29 -0600

    Suppress all csplit output

--- orchestration/temporal-java/scripts/split-hl7-log.sh
+++ orchestration/temporal-java/scripts/split-hl7-log.sh
@@ -6,7 +6,7 @@ prefix=${hl7log%.*}
 
 # Split big NAME.log file on lines with only carriage return
 # Produces many NAME.ddddd files
-csplit --digits 5 --elide-empty-files --suppress-matched --prefix $prefix. $hl7log_fullpath /^$'\r'/ '{*}'
+csplit --digits 5 --elide-empty-files --suppress-matched --silent --prefix $prefix. $hl7log_fullpath /^$'\r'/ '{*}'
 
 # Output all split NAME.ddddd files
 ls $prefix.* | grep -v $hl7log
