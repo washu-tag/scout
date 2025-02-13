6250db15f611f4cc8a02c0e9ddc978177698e166
Andy Lassiter <andrewl@wustl.edu>
2025-02-13 13:41:49 -0700

    Do we need opentracing shim for opentelemetry?

--- orchestration/temporal-java/src/main/java/edu/washu/tag/temporal/config/OTELConfig.java
+++ orchestration/temporal-java/src/main/java/edu/washu/tag/temporal/config/OTELConfig.java
@@ -2,6 +2,9 @@ package edu.washu.tag.temporal.config;
 
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.OpenTelemetry;
+
+import io.opentelemetry.opentracingshim.OpenTracingShim;
+import io.opentracing.Tracer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +14,11 @@ public class OTELConfig {
     @Bean
     public OpenTelemetry openTelemetry() {
         return GlobalOpenTelemetry.get();
+    }
+
+    @Bean
+    public Tracer openTracingTracer(OpenTelemetry openTelemetry) {
+        return OpenTracingShim.createTracerShim(openTelemetry);
     }
 
 }
