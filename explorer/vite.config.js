946a24d02387878662d4692dc40bda720aa1f8ea
Kate Alpert <alpertk@wustl.edu>
2025-02-21 08:19:30 -0700

    Lint

--- explorer/vite.config.js
+++ explorer/vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +9,6 @@ export default defineConfig({
     port: 3000,
     strictPort: true,
     host: true,
-    origin: "http://0.0.0.0:3000",
+    origin: 'http://0.0.0.0:3000',
   },
-})
+});
