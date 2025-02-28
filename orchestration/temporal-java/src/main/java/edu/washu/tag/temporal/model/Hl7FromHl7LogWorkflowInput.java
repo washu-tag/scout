40432736ed3def456595f66d103eaedb64176c56
John Flavin <flavinj@gmail.com>
2025-02-28 11:39:34 -0600

    Heavily refactor workflow around taking log paths as input, which can be directories

--- orchestration/temporal-java/src/main/java/edu/washu/tag/temporal/model/Hl7FromHl7LogWorkflowInput.java
+++ orchestration/temporal-java/src/main/java/edu/washu/tag/temporal/model/Hl7FromHl7LogWorkflowInput.java
@@ -1,3 +1,3 @@
 package edu.washu.tag.temporal.model;
 
-public record Hl7FromHl7LogWorkflowInput(String date, String logsRootPath, String scratchSpaceRootPath, String hl7OutputPath) { }
+public record Hl7FromHl7LogWorkflowInput(String logPath, String scratchSpaceRootPath, String hl7OutputPath) { }
