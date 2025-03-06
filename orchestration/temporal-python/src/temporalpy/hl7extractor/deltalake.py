dd3caf55fcc20fba5231e0be617aceda15821fd0
John Flavin <flavinj@gmail.com>
2025-03-06 16:55:00 -0600

    Rewrite python activity to do everything with spark

--- orchestration/temporal-python/src/temporalpy/hl7extractor/deltalake.py
+++ orchestration/temporal-python/src/temporalpy/hl7extractor/deltalake.py
@@ -1,159 +1,51 @@
-import argparse
-import dataclasses
-import itertools
-import logging
 import os
-import sys
-from collections.abc import Iterator
-from dataclasses import asdict
-from typing import Optional, Iterable
 
-import s3fs
 from delta import configure_spark_with_delta_pip
 from delta.tables import DeltaTable
-from pyspark.sql import SparkSession
+from pyspark.sql import Column, SparkSession
 from pyspark.sql import functions as F
-from pyspark.sql.types import ArrayType, StructType, StructField, StringType
+from temporalio import activity
 from temporalio.exceptions import ApplicationError
-
-from temporalpy.hl7extractor.hl7 import (
-    MessageData,
-    read_hl7_message,
-    extract_data,
-    parse_hl7_message,
-    PatientIdentifier,
-)
-
-log = logging.getLogger(__name__)
-
-
-s3filesystem = None
 
 DATE_FORMAT = "yyyyMMdd"
 DT_FORMAT = "yyyyMMddHHmmss"
 
-
-def dataclass_to_spark_schema(dataclass) -> StructType:
-    """Turn a dataclass into a spark struct of all nullable strings"""
-    return StructType(
-        [
-            StructField(field.name, StringType(), True)
-            for field in dataclasses.fields(dataclass)
-        ]
-    )
+EMPTY_FILTER = " and ".join(
+    f"{field} is not null and {field} != ''"
+    for field in ("id_number", "assigning_authority", "identifier_type_code")
+)
 
 
-MESSAGE_SCHEMA = dataclass_to_spark_schema(MessageData)
-JSON_ARRAY_SCHEMA = ArrayType(dataclass_to_spark_schema(PatientIdentifier))
+def segment_field(
+    segment: str,
+    field: int,
+    component: int = 1,
+    segment_column: Column | str = "segments",
+) -> Column:
+    if segment == "MSH":
+        segment_val = F.split("message", "\\|")
+    else:
+        segment_val = (
+            F.filter(segment_column, lambda s: s["id"] == segment)
+            .getItem(0)
+            .getField("fields")
+        )
+    return F.split(segment_val.getField(field - 1), "\\^").getItem(component - 1)
 
 
-def parse_timestamp_col(col_name: str):
+def parse_timestamp_col(col: Column | str) -> Column:
     """Convert a timestamp column to a datetime column.
     Some of our timestamps are missing seconds, so we need to add them."""
     return F.to_timestamp(
-        F.when(
-            F.length(col_name) == 12, F.concat(F.col(col_name), F.lit("00"))
-        ).otherwise(F.col(col_name)),
+        F.when(F.length(col) == 12, F.concat(col, F.lit("00"))).otherwise(col),
         DT_FORMAT,
     )
-
-
-def read_hl7_directory(
-    directory: str, cache: Optional[set[str]] = None
-) -> Iterator[MessageData]:
-    """Read HL7 messages from directory."""
-    cache = cache or set()
-    if directory in cache:
-        return
-
-    for root, subdir, files in os.walk(directory):
-        for file in files:
-            path = os.path.join(root, file)
-            if path in cache:
-                continue
-            if file.endswith(".hl7"):
-                yield read_hl7_message(path)
-                cache.add(path)
-
-    cache.add(directory)
-
-
-def read_hl7_s3(s3path: str, cache: Optional[set[str]] = None) -> Iterator[MessageData]:
-    """Read HL7 messages from S3."""
-    cache = cache or set()
-    if s3path in cache:
-        return
-
-    global s3filesystem
-    if s3filesystem is None:
-        s3filesystem = s3fs.S3FileSystem()
-
-    def read_hl7_file_from_s3(path: str) -> MessageData:
-        log.info("Reading HL7 message from S3 file %s", path)
-        with s3filesystem.open(path, "rb") as f:
-            try:
-                message = parse_hl7_message(f)
-                log.debug("Successfully read HL7 message from %s", path)
-                return extract_data(message, path)
-            except Exception as e:
-                raise ApplicationError(f"Error extracting {path}") from e
-
-    if s3path.endswith(".hl7"):
-        cache.add(s3path)
-        yield read_hl7_file_from_s3(s3path)
-    else:
-        for p in s3filesystem.glob(s3path + "**.hl7"):
-            if p in cache:
-                continue
-            cache.add(p)
-            yield read_hl7_file_from_s3(p)
-
-
-def read_hl7_input(hl7input: Iterable[str]) -> Iterator[MessageData]:
-    """Read HL7 messages from input files or directories."""
-    cache = set()
-    for path in hl7input:
-        if path in cache:
-            continue
-        if path.startswith("s3://"):
-            yield from read_hl7_s3(path, cache=cache)
-        elif os.path.isdir(path):
-            yield from read_hl7_directory(path, cache=cache)
-        else:
-            yield read_hl7_message(path)
-            cache.add(path)
-
-
-def read_hl7_file_path_file(hl7_file_path_file: str) -> Iterable[str]:
-    """Read HL7 file paths from file."""
-    if hl7_file_path_file.startswith("s3://"):
-        global s3filesystem
-        if s3filesystem is None:
-            s3filesystem = s3fs.S3FileSystem()
-        with s3filesystem.open(hl7_file_path_file, "r") as f:
-            yield from (l for line in f if (l := line.strip()))
-    else:
-        with open(hl7_file_path_file, "r") as f:
-            yield from (l for line in f if (l := line.strip()))
 
 
 def import_hl7_files_to_deltalake(
     delta_table: str, hl7_file_path_files: list[str], modality_map_csv_path: str
 ):
     """Extract data from HL7 messages and write to Delta Lake."""
-    log.info("Reading %d files to find HL7 file paths", len(hl7_file_path_files))
-    hl7_input = list(
-        itertools.chain.from_iterable(
-            read_hl7_file_path_file(hl7_file_path_file)
-            for hl7_file_path_file in hl7_file_path_files
-        )
-    )
-
-    log.info("Reading HL7 messages from %d HL7 input files", len(hl7_input))
-    log.debug("HL7 files: %s", hl7_input)
-
-    if not hl7_input:
-        raise ApplicationError("No HL7 input files or directories provided")
 
     s3a_endpoint = os.environ.get("AWS_ENDPOINT_URL")
     s3a_access_key = os.environ.get("AWS_ACCESS_KEY_ID")
@@ -163,7 +55,7 @@ def import_hl7_files_to_deltalake(
     if not s3a_endpoint or not s3a_access_key or not s3a_secret_key:
         raise ApplicationError("S3 endpoint, access key, and secret key required")
 
-    log.debug("Creating Spark session")
+    activity.logger.info("Creating Spark session")
     spark_builder = (
         SparkSession.builder.appName("IngestHL7ToDeltaLake")
         .config("spark.jars", "/opt/spark/jars/*.jar")
@@ -189,23 +81,28 @@ def import_hl7_files_to_deltalake(
     )
     spark = configure_spark_with_delta_pip(spark_builder).getOrCreate()
 
-    log.debug("Reading HL7 messages")
-    df = spark.createDataFrame(
-        (
-            asdict(message)
-            for message in read_hl7_input(hl7_input)
-            if message is not None
-        ),
-        schema=MESSAGE_SCHEMA,
+    activity.logger.info("Reading %d HL7 file path files", len(hl7_file_path_files))
+    file_path_file_df = spark.read.text(hl7_file_path_files)
+
+    # I wish I could just have spark read these directly without collecting first but I can't figure out how
+    hl7_file_paths_from_spark = [row.value for row in file_path_file_df.collect()]
+    if not hl7_file_paths_from_spark:
+        raise ApplicationError("No HL7 files found in HL7 file path files")
+
+    activity.logger.info("Reading %d HL7 messages", len(hl7_file_paths_from_spark))
+    df = (
+        spark.read.format("hl7")
+        .load(hl7_file_paths_from_spark)
+        .withColumn("source_file", F.input_file_name())
     )
 
     if df.isEmpty():
         raise ApplicationError("No data extracted from HL7 messages")
 
-    log.info(f"Extracted data from {df.count()} HL7 messages")
+    activity.logger.info(f"Extracted data from {df.count()} HL7 messages")
 
     # Read modality map
-    log.info("Reading modality map from %s", modality_map_csv_path)
+    activity.logger.info("Reading modality map from %s", modality_map_csv_path)
     modality_map = (
         spark.read.option("header", True)
         .csv(modality_map_csv_path)
@@ -215,83 +112,131 @@ def import_hl7_files_to_deltalake(
         )
     )
 
-    # Extract patient_id_json into distinct columns
-    patient_id_df = (
-        df.select("message_control_id", "patient_id_json")
-        .withColumn(
-            "patient_id",
-            F.explode(F.from_json(F.col("patient_id_json"), JSON_ARRAY_SCHEMA)),
+    activity.logger.info("Creating report df")
+    report_df = (
+        df.select(
+            "source_file",
+            # Get all OBX segments and explode into separate rows, keeping the index as "pos" column
+            F.posexplode(F.filter("segments", lambda s: s["id"] == "OBX")),
         )
-        .withColumn(
-            "patient_id_col_name",
+        .select(
+            "source_file",
+            # "pos" holds the index of the OBX segment
+            "pos",
+            # "col" holds the OBX segment data
+            F.when(
+                # TX data type has a ~ delimiter, replace with newline
+                F.col("col").getField("fields").getItem(1) == "TX",
+                F.regexp_replace(F.col("col").getField("fields").getItem(4), "~", "\n"),
+            )
+            # Other data types are a single line as the value
+            .otherwise(F.col("col").getField("fields").getItem(4)).alias("obx-5"),
+            F.col("col").getField("fields").getItem(10).alias("obx-11"),
+        )
+        .groupby("source_file")
+        .agg(
+            # Join all lines of report text into one string
+            F.concat_ws("\n", F.collect_list("obx-5")).alias("report_text"),
+            # Assume report statuses are the same, pick first
+            F.first("obx-11").alias("report_status"),
+        )
+    )
+
+    activity.logger.info("Extracting patient id columns")
+    exploded_patient_id_df = df.select(
+        "source_file",
+        F.explode(
+            F.split(
+                F.filter("segments", lambda s: s["id"] == "PID")
+                .getItem(0)
+                .getField("fields")
+                .getItem(2),
+                "~",
+            )
+        ).alias("pid"),
+    ).select(
+        "source_file",
+        F.split("pid", "\\^").getItem(0).alias("id_number"),
+        F.split("pid", "\\^").getItem(3).alias("assigning_authority"),
+        F.split("pid", "\\^").getItem(4).alias("identifier_type_code"),
+    )
+
+    patient_id_df = (
+        # Filter out patient ids with missing fields
+        exploded_patient_id_df.filter(EMPTY_FILTER)
+        .select(
+            "source_file",
+            "id_number",
             F.concat_ws(
                 "_",
-                F.lower(F.col("patient_id.assigning_authority")),
-                F.lower(F.col("patient_id.identifier_type_code")),
-            ),
+                F.lower("assigning_authority"),
+                F.lower("identifier_type_code"),
+            ).alias("patient_id_col_name"),
         )
-        .groupBy("message_control_id")
+        .groupBy("source_file")
+        # Turn the patient_id_col_name values into columns
         .pivot("patient_id_col_name")
         # Assume they only have one patient id for each type
-        .agg(F.first("patient_id.id_number"))
+        .agg(F.first("id_number"))
     )
-    if log.isEnabledFor(logging.DEBUG):
-        # TODO Is it ok to log the types of patient ids?
-        #   We definitely aren't logging any specific values, just what kinds of ids exist.
-        log.debug(
-            "Extracted patient ids from HL7 messages. Cols: %s",
-            ", ".join(c for c in patient_id_df.columns if c != "message_control_id"),
-        )
+    # Note that we do not filter out any patient ids here, so they are all available in the JSON
+    patient_id_json_df = exploded_patient_id_df.groupBy("source_file").agg(
+        F.to_json(
+            F.collect_list(
+                F.struct("id_number", "assigning_authority", "identifier_type_code")
+            )
+        ).alias("patient_id_json")
+    )
 
-    # Join data with modality map and exploded patient ids
+    activity.logger.info("Joining data into report df")
     df = (
-        df.join(modality_map, "service_identifier", "left")
-        .select(
-            "modality",
-            "message_control_id",
-            parse_timestamp_col("message_dt").alias("message_dt"),
-            F.to_date(
-                F.regexp_extract("birth_date", r"^(\d{8})", 1), DATE_FORMAT
-            ).alias("birth_date"),
-            "sex",
-            "race",
-            "zip_or_postal_code",
-            "country",
-            "ethnic_group",
-            "orc_2_placer_order_number",
-            "obr_2_placer_order_number",
-            "orc_3_filler_order_number",
-            "obr_3_filler_order_number",
-            "service_identifier",
-            "service_name",
-            "service_coding_system",
-            parse_timestamp_col("requested_dt").alias("requested_dt"),
-            parse_timestamp_col("observation_dt").alias("observation_dt"),
-            parse_timestamp_col("observation_end_dt").alias("observation_end_dt"),
-            parse_timestamp_col("results_report_status_change_dt").alias(
+        df.select(
+            segment_field("MSH", 10).alias("message_control_id"),
+            segment_field("MSH", 4).alias("sending_facility"),
+            segment_field("MSH", 12).alias("version_id"),
+            parse_timestamp_col(segment_field("MSH", 7)).alias("message_dt"),
+            F.to_date(F.substring(segment_field("PID", 7), 1, 8), DATE_FORMAT).alias(
+                "birth_date"
+            ),
+            segment_field("PID", 8).alias("sex"),
+            segment_field("PID", 10).alias("race"),
+            segment_field("PID", 11, 5).alias("zip_or_postal_code"),
+            segment_field("PID", 11, 6).alias("country"),
+            segment_field("PID", 22).alias("ethnic_group"),
+            segment_field("ORC", 2).alias("orc_2_placer_order_number"),
+            segment_field("OBR", 2).alias("obr_2_placer_order_number"),
+            segment_field("ORC", 3).alias("orc_3_filler_order_number"),
+            segment_field("OBR", 3).alias("obr_3_filler_order_number"),
+            segment_field("OBR", 4, 1).alias("service_identifier"),
+            segment_field("OBR", 4, 2).alias("service_name"),
+            segment_field("OBR", 4, 3).alias("service_coding_system"),
+            parse_timestamp_col(segment_field("OBR", 6)).alias("requested_dt"),
+            parse_timestamp_col(segment_field("OBR", 7)).alias("observation_dt"),
+            parse_timestamp_col(segment_field("OBR", 8)).alias("observation_end_dt"),
+            parse_timestamp_col(segment_field("OBR", 22)).alias(
                 "results_report_status_change_dt"
             ),
-            "diagnostic_service_id",
-            "report_text",
-            "report_status",
-            "obx_11_observation_result_status",
-            "diagnosis_code",
-            "diagnosis_code_text",
-            "diagnosis_code_coding_system",
-            "study_instance_uid",
-            "sending_facility",
-            "version_id",
+            segment_field("OBR", 24).alias("diagnostic_service_id"),
+            segment_field("DG1", 3, 1).alias("diagnosis_code"),
+            segment_field("DG1", 3, 2).alias("diagnosis_code_text"),
+            segment_field("DG1", 3, 3).alias("diagnosis_code_coding_system"),
+            segment_field("ZDS", 1).alias("study_instance_uid"),
             "source_file",
-            "patient_id_json",
         )
+        .join(report_df, "source_file", "left")
+        .join(patient_id_df, "source_file", "left")
+        .join(patient_id_json_df, "source_file", "left")
+        .join(modality_map, "service_identifier", "left")
         .withColumn("year", F.year("message_dt"))
         .withColumn("updated", F.current_timestamp())
-        .join(patient_id_df, "message_control_id", "left")
     )
 
     # Create table if it doesn't yet exist
     delta_table = delta_table.replace("s3://", "s3a://")
 
+    activity.logger.info(
+        "Creating Delta Lake table %s if it does not exist", delta_table
+    )
     dt = (
         DeltaTable.createIfNotExists(spark)
         .location(delta_table)
@@ -300,85 +245,19 @@ def import_hl7_files_to_deltalake(
         .execute()
     )
 
-    log.info(f"Writing data to Delta Lake table {delta_table}")
+    activity.logger.info("Writing data to Delta Lake table %s", delta_table)
     (
         dt.alias("s")
         .merge(
             df.alias("t"),
-            "s.message_control_id = t.message_control_id AND s.year = t.year",
+            "s.source_file = t.source_file AND s.year = t.year",
         )
         .whenMatchedUpdateAll()
         .whenNotMatchedInsertAll()
         .execute()
     )
 
+    activity.logger.info("Stopping spark")
+    spark.stop()
 
-def delete_delta_table(delta_table: str) -> None:
-    """Delete Delta Lake table."""
-    log.info(f"Deleting Delta Lake table {delta_table}")
-    dt = DeltaTable(delta_table)
-    dt.delete()
-    dt.vacuum()
-
-
-def main_cli(argv=None) -> int:
-    """Main entry point for the CLI."""
-    if argv is None:
-        argv = sys.argv[1:]
-
-    parser = argparse.ArgumentParser(
-        description="Extract data from HL7 messages and write to Delta Lake",
-    )
-    parser.add_argument(
-        "--delete",
-        help="Delete Delta Lake table",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--debug",
-        help="Turn on debug logging",
-        action="store_true",
-    )
-    parser.add_argument(
-        "delta_table",
-        help="Path to Delta Lake table",
-    )
-    parser.add_argument(
-        "modality_map_csv_path",
-        help="Path to modality map CSV file",
-    )
-    parser.add_argument(
-        "hl7_input",
-        help="HL7 input files or directories",
-        nargs="+",
-    )
-
-    args = parser.parse_args(argv)
-    if args.delete:
-        delete_delta_table(args.delta_table)
-        return 0
-
-    if args.debug:
-        logging.basicConfig(
-            level=logging.DEBUG,
-            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        )
-    else:
-        logging.basicConfig(
-            level=logging.INFO,
-            format="%(message)s",
-        )
-
-    try:
-        import_hl7_files_to_deltalake(
-            args.delta_table, args.hl7_input, args.modality_map_csv_path
-        )
-        log.info("success")
-        return 0
-    except Exception as e:
-        log.exception("Error extracting HL7 messages", exc_info=e)
-        return 1
-
-
-if __name__ == "__main__":
-    sys.exit(main_cli())
+    activity.logger.info("Finished")
