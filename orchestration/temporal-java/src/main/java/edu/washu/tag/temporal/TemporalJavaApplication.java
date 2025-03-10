1820671d3789fb8410567747ee091ff4341d5083
John Flavin <flavinj@gmail.com>
2025-03-10 12:27:23 -0500

    Fix tabs in test files

--- orchestration/temporal-java/src/main/java/edu/washu/tag/temporal/TemporalJavaApplication.java
+++ orchestration/temporal-java/src/main/java/edu/washu/tag/temporal/TemporalJavaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TemporalJavaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TemporalJavaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TemporalJavaApplication.class, args);
+    }
 
 }
