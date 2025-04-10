36341ff65fa47fe11914fa7dff17ff35bd75e0fc
Kate Alpert <alpertk@wustl.edu>
2025-04-10 12:16:43 -0600

    Remove stack trace from REST API response (#66)
    * Remove stack trace from REST API response

--- orchestration/temporal-python/src/temporalpy/healthapi.py
+++ orchestration/temporal-python/src/temporalpy/healthapi.py
@@ -14,7 +14,7 @@ SPARK_HEALTH_TEMP_FILE = Path(tempfile.mkstemp()[1])
 HEALTHY = "healthy"
 UNHEALTHY = "unhealthy"
 HEALTHY_JSON_RESPONSE = JSONResponse(status_code=200, content={"status": HEALTHY})
-
+HEALTH_CHECK_EXCEPTION_MESSAGE = "Health check failed with exception"
 
 app = FastAPI()
 
@@ -40,8 +40,8 @@ async def healthz():
         log.warning('Health check file reports failure: "%s"', reason)
         return unhealthy_json_response(reason, messages)
     except Exception as e:
-        log.error("Health check failed with exception", e)
-        return unhealthy_json_response(str(e))
+        log.exception(HEALTH_CHECK_EXCEPTION_MESSAGE)
+        return unhealthy_json_response(HEALTH_CHECK_EXCEPTION_MESSAGE)
 
 
 def unhealthy_json_response(
