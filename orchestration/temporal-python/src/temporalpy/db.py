1cb23f7c6361e149aa57678ab63f4b979a9038d8
John Flavin <flavinj@gmail.com>
2025-04-22 11:37:53 -0500

    Read postgres connection info from env

--- orchestration/temporal-python/src/temporalpy/db.py
+++ orchestration/temporal-python/src/temporalpy/db.py
@@ -1,54 +1,77 @@
+import os
+
 import psycopg
+
+
+_connection = None
+
+
+def connect_to_db():
+    """Connect to the PostgreSQL database."""
+    global _connection
+    if _connection is None:
+        # Load database configuration from environment variables
+        db_host = os.getenv("DB_HOST")
+        db_port = os.getenv("DB_PORT", "5432")
+        db_name = os.getenv("DB_NAME")
+        db_user = os.getenv("DB_USER")
+        db_password = os.getenv("DB_PASSWORD")
+        _connection = psycopg.connect(
+            host=db_host,
+            port=db_port,
+            database=db_name,
+            user=db_user,
+            password=db_password,
+        )
+    return _connection
 
 
 def write_errors(
     hl7_files: list[str], error_message: str, workflow_id: str, activity_id: str
 ) -> None:
     """Write an error message to the database for a list of HL7 files."""
-    # TODO Get the database connection info from environment variables
-    with psycopg.connect() as conn:
-        with conn.cursor() as cursor:
-            # Find existing rows
-            cursor.execute(
-                """
-                SELECT DISTINCT file_path, log_file_path, segment_number
-                FROM hl7_files
-                WHERE file_path in %s
-                """,
-                (hl7_files,),
+    conn = connect_to_db()
+    with conn.cursor() as cursor:
+        # Find existing rows
+        cursor.execute(
+            """
+            SELECT DISTINCT file_path, log_file_path, segment_number
+            FROM hl7_files
+            WHERE file_path in %s
+            """,
+            (hl7_files,),
+        )
+        rows = cursor.fetchall()
+
+        # Did we find rows for all the files that were passed in?
+        found_hl7_files = {row[0] for row in rows}
+        input_hl7_files = set(hl7_files)
+        missing_hl7_files = input_hl7_files - found_hl7_files
+
+        # Use the existing log file and segment number for the found HL7 files, placeholders for the missing ones
+        insert_rows = [
+            (
+                log_file_path,
+                segment_number,
+                hl7_file,
+                "failed",
+                error_message,
+                workflow_id,
+                activity_id,
             )
-            rows = cursor.fetchall()
+            for hl7_file, log_file_path, segment_number in rows
+        ] + [
+            (None, -1, hl7_file, "failed", error_message, workflow_id, activity_id)
+            for hl7_file in missing_hl7_files
+        ]
 
-            # Did we find rows for all the files that were passed in?
-            found_hl7_files = {row[0] for row in rows}
-            input_hl7_files = set(hl7_files)
-            missing_hl7_files = input_hl7_files - found_hl7_files
-
-            # Use the existing log file and segment number for the found HL7 files, placeholders for the missing ones
-            insert_rows = [
-                (
-                    log_file_path,
-                    segment_number,
-                    hl7_file,
-                    "failed",
-                    error_message,
-                    workflow_id,
-                    activity_id,
-                )
-                for hl7_file, log_file_path, segment_number in rows
-            ] + [
-                (None, -1, hl7_file, "failed", error_message, workflow_id, activity_id)
-                for hl7_file in missing_hl7_files
-            ]
-
-            # Write new rows
-            cursor.executemany(
-                """
-                INSERT INTO hl7_files (log_file_path, segment_number, file_path, status, error_message, workflow_id, activity_id)
-                VALUES (%s, %s, %s, %s, %s, %s, %s)
-                """,
-                insert_rows,
-            )
+        # Write new rows
+        cursor.executemany(
+            """
+            INSERT INTO hl7_files (log_file_path, segment_number, file_path, status, error_message, workflow_id, activity_id)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+            """,
+            insert_rows,
+        )
 
         conn.commit()
-        conn.close()
