d321da7befa6ba20a4f169385323f6d2406515a7
John Flavin <flavinj@gmail.com>
2025-04-29 12:52:10 -0500

    Follow flyway convention: do not modify existing scripts, make a new script with a higher version number

--- extractor/hl7log-extractor/src/main/resources/db/migration/V3__Create_views.sql
+++ extractor/hl7log-extractor/src/main/resources/db/migration/V3__Create_views.sql
@@ -15,11 +15,5 @@ FROM hl7_files h
 WHERE processed_at = (
     SELECT MAX(processed_at)
     FROM hl7_files
-    WHERE file_path = h.file_path OR
-        (
-            file_path IS NULL AND
-            h.file_path IS NULL AND
-            log_file_path = h.log_file_path AND
-            segment_number = h.segment_number
-        )
+    WHERE file_path = h.file_path
 );
