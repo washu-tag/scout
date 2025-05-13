e91837e5909c46de71a64590272dcbd306518310
Kate Alpert <alpertk@wustl.edu>
2025-05-13 13:38:15 -0600

    Remove attempt to unit test an exception on file stat

--- extractor/hl7log-extractor/src/test/java/edu/washu/tag/extractor/hl7log/FindLogFileTest.java
+++ extractor/hl7log-extractor/src/test/java/edu/washu/tag/extractor/hl7log/FindLogFileTest.java
@@ -15,7 +15,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.Set;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -38,17 +37,16 @@ class FindLogFileTest {
             .setActivityImplementations(new FindHl7LogsActivityImpl(fileHandler))
             .build();
 
-    private Path match;
-    private Path mismatch;
-    private Path hidden;
-    private Path notLog;
+    private final String date = "20250105";
+    private Path matchingDateLog;
+    private Path alernativeDateLog;
 
     @BeforeEach
     void setUp() throws IOException {
-        match = Files.createFile(tempDir.resolve("20250105.log"));
-        mismatch = Files.createFile(tempDir.resolve("20250106.log"));
-        hidden = Files.createFile(tempDir.resolve(".20250105.log.asef"));
-        notLog = Files.createFile(tempDir.resolve("20250105.txt"));
+        matchingDateLog = Files.createFile(tempDir.resolve(date + ".log"));
+        alernativeDateLog = Files.createFile(tempDir.resolve("20250106.log"));
+        Files.createFile(tempDir.resolve("." + date + ".log.asef"));
+        Files.createFile(tempDir.resolve("20250105.txt"));
     }
 
     @Test
@@ -63,8 +61,7 @@ class FindLogFileTest {
 
         FindHl7LogFileOutput out = activityStub.findHl7LogFiles(input);
 
-        assertThat(out.logFiles(), Matchers.containsInAnyOrder(List.of(match.toString(), mismatch.toString()),
-            "Only log files should be returned"));
+        assertThat(out.logFiles(), Matchers.containsInAnyOrder(matchingDateLog.toString(), alernativeDateLog.toString()));
         assertNull(out.continued(), "No Continue-As-New because file count < concurrency");
     }
 
@@ -72,7 +69,7 @@ class FindLogFileTest {
     void testFindLogFiles_excludesFilesCorrectlyWithDate(FindHl7LogsActivity activityStub) {
         FindHl7LogFileInput input = new FindHl7LogFileInput(
             List.of(tempDir.toString()),
-            "20250105",
+            date,
             null,
             null,
             Integer.MAX_VALUE
@@ -80,31 +77,7 @@ class FindLogFileTest {
 
         FindHl7LogFileOutput out = activityStub.findHl7LogFiles(input);
 
-        assertEquals(List.of(match.toString()), out.logFiles(), "Only the matching date file should be returned");
-        assertNull(out.continued(), "No Continue-As-New because file count < concurrency");
-    }
-
-    @Test
-    void testFindLogFiles_visitFileFailed(FindHl7LogsActivity activityStub) throws Exception {
-        // File that will raise AccessDeniedException
-        Path noPerms = Files.createFile(tempDir.resolve("privateFile"));
-        Files.setPosixFilePermissions(noPerms, Set.of());   // chmod 000
-
-        // Dir that will raise AccessDeniedException
-        Path noPermsDir = Files.createFile(tempDir.resolve("privateDir"));
-        Files.setPosixFilePermissions(noPermsDir, Set.of());   // chmod 000
-
-        FindHl7LogFileInput input = new FindHl7LogFileInput(
-            List.of(tempDir.toString()),
-            "20250105",
-            null,
-            null,
-            Integer.MAX_VALUE
-        );
-
-        FindHl7LogFileOutput out = activityStub.findHl7LogFiles(input);
-
-        assertEquals(List.of(match.toString()), out.logFiles(), "Only the matching date file should be returned");
+        assertEquals(List.of(matchingDateLog.toString()), out.logFiles(), "Only the matching date file should be returned");
         assertNull(out.continued(), "No Continue-As-New because file count < concurrency");
     }
 }
