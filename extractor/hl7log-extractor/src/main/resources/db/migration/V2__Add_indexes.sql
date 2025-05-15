581f3a215f3cb02d81f490a27f7873a549d878b4
John Flavin <flavinj@gmail.com>
2025-05-15 15:04:57 -0500

    Add index idx_file_statuses_file_path_type

--- extractor/hl7log-extractor/src/main/resources/db/migration/V2__Add_indexes.sql
+++ extractor/hl7log-extractor/src/main/resources/db/migration/V2__Add_indexes.sql
@@ -1,5 +1,6 @@
 -- Indexes for file_statuses
 CREATE INDEX idx_file_statuses_file_path ON file_statuses (file_path);
+CREATE INDEX idx_file_statuses_file_path_type ON file_statuses (file_path, type);
 
 -- Indexes for hl7_files
 CREATE INDEX idx_hl7_files_log_file_path ON hl7_files (log_file_path);
