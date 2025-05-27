1032f79cfcac969c890ac0692c68efacfb2e869e
John Flavin <flavinj@gmail.com>
2025-05-27 11:07:54 -0500

    Refactor spark for performance (#123)
    * Refactor spark processing to inline functions
    
    * Fix filter calls with lambdas; turn them into expr strings
    
    * Fix exploding patient ids
    
    * Adjust some logging and heartbeats
    
    * Add caching for reused dataframes
    
    * Set some spark.sql.adaptive config params
    
    * Set some serialization params
    
    * Tweak logging about downloading zips
    
    * Modify spark params, and provide justifications for ones we keep
    
    * Remove spark config options. They seem to not have an effect.
    
    * Write success messages after closing spark
    
    * Tweak error logging during finally block
    
    * Clear spark cache in finally block before closing

--- extractor/hl7-transformer/src/hl7scout/hl7extractor/deltalake.py
+++ extractor/hl7-transformer/src/hl7scout/hl7extractor/deltalake.py
@@ -1,13 +1,14 @@
 # I don't know why we have to import this specific exception type, since it is
 # literally just an alias for the builtin TimeoutError.
 # But if we try to catch the builtin TimeoutError, it doesn't work.
+import logging
 from concurrent.futures._base import TimeoutError
 from collections import defaultdict
 from pathlib import Path
 
 from delta.tables import DeltaTable
 from py4j.protocol import Py4JError
-from pyspark.sql import Column, SparkSession
+from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 from s3fs import S3FileSystem
 from temporalio import activity
@@ -19,40 +20,6 @@ import os
 import shutil
 import tempfile
 import zipfile
-
-DATE_FORMAT = "yyyyMMdd"
-DT_FORMAT = "yyyyMMddHHmmss"
-
-EMPTY_FILTER = " and ".join(
-    f"{field} is not null and {field} != ''"
-    for field in ("id_number", "assigning_authority", "identifier_type_code")
-)
-
-
-def segment_field(
-    segment: str,
-    field: int,
-    component: int = 1,
-    segment_column: Column | str = "segments",
-) -> Column:
-    if segment == "MSH":
-        segment_val = F.split("message", "\\|")
-    else:
-        segment_val = (
-            F.filter(segment_column, lambda s: s["id"] == segment)
-            .getItem(0)
-            .getField("fields")
-        )
-    return F.split(segment_val.getField(field - 1), "\\^").getItem(component - 1)
-
-
-def parse_timestamp_col(col: Column | str) -> Column:
-    """Convert a timestamp column to a datetime column.
-    Some of our timestamps are missing seconds, so we need to add them."""
-    return F.to_timestamp(
-        F.when(F.length(col) == 12, F.concat(col, F.lit("00"))).otherwise(col),
-        DT_FORMAT,
-    )
 
 
 def parse_s3_zip_paths(hl7_file_paths: list[str]) -> dict[str, list[str]]:
@@ -86,8 +53,12 @@ def import_hl7_files_to_deltalake(
     health_file: Path,
 ) -> int:
     """Extract data from HL7 messages and write to Delta Lake."""
+    activity_info = activity.info()
+    workflow_id = activity_info.workflow_id
+    activity_id = activity_info.activity_id
     spark = None
     temp_dir = None
+    success_paths = []
     try:
         activity.logger.info("Creating Spark session")
         spark = (
@@ -108,10 +79,10 @@ def import_hl7_files_to_deltalake(
             raise ApplicationError("No HL7 files found in HL7 file path files")
 
         activity.heartbeat()
-        activity.logger.info("Downloading and extracting HL7 files")
 
         temp_dir = tempfile.mkdtemp()
         zip_map = parse_s3_zip_paths(zipped_hl7_file_paths_from_spark)
+        activity.logger.info("Downloading and extracting %d zip files", len(zip_map))
         temp_to_s3 = download_and_extract_zips(zip_map, temp_dir)
         temp_hl7_files = [temp_path for temp_path, _ in temp_to_s3]
 
@@ -139,32 +110,85 @@ def import_hl7_files_to_deltalake(
         df = df.join(temp_to_s3_df, on="temp_path", how="left")
         df = df.drop("temp_path")
 
+        activity.heartbeat()
+        # Extract the HL7 segments from the smolder objects
+        df = (
+            df.select(
+                "source_file",
+                # MSH is stored in its own "message" column
+                F.split("message", "\\|").alias("msh"),
+                # Other segments are stored in objects in the "segments" column.
+                # We need to find the ones we want using their "id" field;
+                # for most of them we use the first item in the list.
+                *[
+                    F.expr(
+                        f"filter(segments, x -> x.id = '{segment}')[0].fields"
+                    ).alias(segment.lower())
+                    for segment in ("PID", "ORC", "OBR", "DG1", "ZDS")
+                ],
+                # OBX is a special case; we need to keep it as a list for now so
+                # later we can explode it into separate rows
+                F.expr("filter(segments, x -> x.id = 'OBX')").alias("obx_lines"),
+            )
+            .select(
+                "source_file",
+                "obx_lines",
+                # Extract all the fields where we only want the first component
+                *[
+                    F.split(F.col(segment).getItem(field - 1), "\\^")
+                    .getItem(0)
+                    .alias(f"{segment}-{field}")
+                    for segment, fields in (
+                        ("msh", (4, 7, 10, 12)),
+                        ("pid", (7, 8, 10, 22)),
+                        ("orc", (2, 3)),
+                        ("obr", (2, 3, 6, 7, 8, 22, 24)),
+                        ("zds", (1,)),
+                    )
+                    for field in fields
+                ],
+                # Extract all the fields where we want multiple components
+                *[
+                    F.split(F.col(segment).getItem(field - 1), "\\^")
+                    .getItem(component - 1)
+                    .alias(f"{segment}-{field}-{component}")
+                    for segment, field, components in (
+                        ("pid", 11, (5, 6)),
+                        ("obr", 4, (1, 2, 3)),
+                        ("dg1", 3, (1, 2, 3)),
+                    )
+                    for component in components
+                ],
+                # PID-2 is a special case.
+                # We keep it as-is for now so we can explode it later.
+                F.col("pid").getItem(2).alias("pid-2"),
+            )
+            .cache()
+        )
+
         # Filter out rows from unparsable HL7 files
-        message_control_id = segment_field("MSH", 10)
-        error_paths = [
-            row.source_file
-            for row in df.filter(message_control_id.isNull())
-            .select("source_file")
-            .collect()
-        ]
+        null_message_ids = (
+            df.filter(F.col("msh-10").isNull()).select("source_file").collect()
+        )
+        error_paths = [row.source_file for row in null_message_ids]
         if error_paths:
             # Write error paths to db
             write_errors(
                 error_paths,
                 "File is not parsable as HL7",
-                activity.info().workflow_id,
-                activity.info().activity_id,
+                workflow_id,
+                activity_id,
             )
 
             # Remove empty / unparsable rows from df
-            df = df.filter(message_control_id.isNotNull())
+            df = df.filter(F.col("msh-10").isNotNull())
 
         if df.isEmpty():
             raise ApplicationError("No data extracted from HL7 messages")
 
-        num_hl7 = df.count()
         activity.heartbeat()
-        activity.logger.info("Extracted data from %d HL7 messages", num_hl7)
+        if activity.logger.isEnabledFor(logging.INFO):
+            activity.logger.info("Extracted data from %d HL7 messages", df.count())
 
         # Read modality map
         modality_map_csv_path = modality_map_csv_path.replace("s3://", "s3a://")
@@ -180,27 +204,28 @@ def import_hl7_files_to_deltalake(
 
         activity.heartbeat()
         activity.logger.info("Creating report df")
+        # We need to explode the OBX segments into separate rows,
+        # then get the report text from the OBX-5 field and status from OBX-11
         report_df = (
             df.select(
                 "source_file",
                 # Get all OBX segments and explode into separate rows, keeping the index as "pos" column
-                F.posexplode(F.filter("segments", lambda s: s["id"] == "OBX")),
+                F.posexplode(F.col("obx_lines")),
             )
+            .withColumn("obx", F.col("col").getField("fields"))
             .select(
                 "source_file",
                 # "pos" holds the index of the OBX segment
                 "pos",
-                # "col" holds the OBX segment data
+                # "obx" holds the OBX segment data
                 F.when(
                     # TX data type has a ~ delimiter, replace with newline
-                    F.col("col").getField("fields").getItem(1) == "TX",
-                    F.regexp_replace(
-                        F.col("col").getField("fields").getItem(4), "~", "\n"
-                    ),
+                    F.col("obx").getItem(1) == "TX",
+                    F.regexp_replace(F.col("obx").getItem(4), "~", "\n"),
                 )
                 # Other data types are a single line as the value
-                .otherwise(F.col("col").getField("fields").getItem(4)).alias("obx-5"),
-                F.col("col").getField("fields").getItem(10).alias("obx-11"),
+                .otherwise(F.col("obx").getItem(4)).alias("obx-5"),
+                F.col("obx").getItem(10).alias("obx-11"),
             )
             .groupby("source_file")
             .agg(
@@ -213,28 +238,40 @@ def import_hl7_files_to_deltalake(
 
         activity.heartbeat()
         activity.logger.info("Extracting patient id columns")
-        exploded_patient_id_df = df.select(
-            "source_file",
-            F.explode(
-                F.split(
-                    F.filter("segments", lambda s: s["id"] == "PID")
-                    .getItem(0)
-                    .getField("fields")
-                    .getItem(2),
-                    "~",
-                )
-            ).alias("pid"),
-        ).select(
-            "source_file",
-            F.split("pid", "\\^").getItem(0).alias("id_number"),
-            F.split("pid", "\\^").getItem(3).alias("assigning_authority"),
-            F.split("pid", "\\^").getItem(4).alias("identifier_type_code"),
-        )
+        exploded_patient_id_df = (
+            df.select(
+                "source_file",
+                # Get the patient id segment and explode into separate rows
+                F.explode(F.split(F.col("pid-2"), "~")).alias("pid-2"),
+            )
+            .select(
+                "source_file",
+                # Split the patient id fields from the exploded pid
+                F.split(F.col("pid-2"), "\\^").alias("patient_ids"),
+            )
+            .select(
+                "source_file",
+                # Get the patient id fields we want
+                F.col("patient_ids").getItem(0).alias("id_number"),
+                F.col("patient_ids").getItem(3).alias("assigning_authority"),
+                F.col("patient_ids").getItem(4).alias("identifier_type_code"),
+            )
+        ).cache()
 
         activity.heartbeat()
+        activity.logger.info("Creating patient id df")
         patient_id_df = (
             # Filter out patient ids with missing fields
-            exploded_patient_id_df.filter(EMPTY_FILTER)
+            exploded_patient_id_df.filter(
+                " and ".join(
+                    f"{field} is not null and {field} != ''"
+                    for field in (
+                        "id_number",
+                        "assigning_authority",
+                        "identifier_type_code",
+                    )
+                )
+            )
             .select(
                 "source_file",
                 "id_number",
@@ -253,6 +290,7 @@ def import_hl7_files_to_deltalake(
 
         # Note that we do not filter out any patient ids here, so they are all available in the JSON
         activity.heartbeat()
+        activity.logger.info("Creating patient id JSON column")
         patient_id_json_df = exploded_patient_id_df.groupBy("source_file").agg(
             F.to_json(
                 F.collect_list(
@@ -262,41 +300,51 @@ def import_hl7_files_to_deltalake(
         )
 
         activity.heartbeat()
-        activity.logger.info("Joining data into report df")
+        activity.logger.info("Building final report DataFrame")
         df = (
             df.select(
-                segment_field("MSH", 10).alias("message_control_id"),
-                segment_field("MSH", 4).alias("sending_facility"),
-                segment_field("MSH", 12).alias("version_id"),
-                parse_timestamp_col(segment_field("MSH", 7)).alias("message_dt"),
+                # Assign human-readable names
+                F.col("msh-10").alias("message_control_id"),
+                F.col("msh-4").alias("sending_facility"),
+                F.col("msh-12").alias("version_id"),
+                F.col("pid-8").alias("sex"),
+                F.col("pid-10").alias("race"),
+                F.col("pid-11-5").alias("zip_or_postal_code"),
+                F.col("pid-11-6").alias("country"),
+                F.col("pid-22").alias("ethnic_group"),
+                F.col("orc-2").alias("orc_2_placer_order_number"),
+                F.col("obr-2").alias("obr_2_placer_order_number"),
+                F.col("orc-3").alias("orc_3_filler_order_number"),
+                F.col("obr-3").alias("obr_3_filler_order_number"),
+                F.col("obr-4-1").alias("service_identifier"),
+                F.col("obr-4-2").alias("service_name"),
+                F.col("obr-4-3").alias("service_coding_system"),
+                F.col("obr-24").alias("diagnostic_service_id"),
+                F.col("dg1-3-1").alias("diagnosis_code"),
+                F.col("dg1-3-2").alias("diagnosis_code_text"),
+                F.col("dg1-3-3").alias("diagnosis_code_coding_scheme"),
+                F.col("zds-1").alias("study_instance_uid"),
+                # Create date and time objects
                 F.to_date(
-                    F.substring(segment_field("PID", 7), 1, 8), DATE_FORMAT
+                    F.substring(F.col("pid-7"), 1, 8),
+                    "yyyyMMdd",
                 ).alias("birth_date"),
-                segment_field("PID", 8).alias("sex"),
-                segment_field("PID", 10).alias("race"),
-                segment_field("PID", 11, 5).alias("zip_or_postal_code"),
-                segment_field("PID", 11, 6).alias("country"),
-                segment_field("PID", 22).alias("ethnic_group"),
-                segment_field("ORC", 2).alias("orc_2_placer_order_number"),
-                segment_field("OBR", 2).alias("obr_2_placer_order_number"),
-                segment_field("ORC", 3).alias("orc_3_filler_order_number"),
-                segment_field("OBR", 3).alias("obr_3_filler_order_number"),
-                segment_field("OBR", 4, 1).alias("service_identifier"),
-                segment_field("OBR", 4, 2).alias("service_name"),
-                segment_field("OBR", 4, 3).alias("service_coding_system"),
-                parse_timestamp_col(segment_field("OBR", 6)).alias("requested_dt"),
-                parse_timestamp_col(segment_field("OBR", 7)).alias("observation_dt"),
-                parse_timestamp_col(segment_field("OBR", 8)).alias(
-                    "observation_end_dt"
-                ),
-                parse_timestamp_col(segment_field("OBR", 22)).alias(
-                    "results_report_status_change_dt"
-                ),
-                segment_field("OBR", 24).alias("diagnostic_service_id"),
-                segment_field("DG1", 3, 1).alias("diagnosis_code"),
-                segment_field("DG1", 3, 2).alias("diagnosis_code_text"),
-                segment_field("DG1", 3, 3).alias("diagnosis_code_coding_system"),
-                segment_field("ZDS", 1).alias("study_instance_uid"),
+                *[
+                    F.to_timestamp(
+                        F.when(
+                            F.length(timestamp_col) == 12,
+                            F.concat(timestamp_col, F.lit("00")),
+                        ).otherwise(timestamp_col),
+                        "yyyyMMddHHmmss",
+                    ).alias(alias)
+                    for timestamp_col, alias in (
+                        ("msh-7", "message_dt"),
+                        ("obr-6", "requested_dt"),
+                        ("obr-7", "observation_dt"),
+                        ("obr-8", "observation_end_dt"),
+                        ("obr-22", "results_report_status_change_dt"),
+                    )
+                ],
                 "source_file",
             )
             .join(report_df, "source_file", "left")
@@ -333,14 +381,11 @@ def import_hl7_files_to_deltalake(
             .execute()
         )
 
+        activity.logger.info("Finished writing to delta lake")
+
         activity.heartbeat()
         success_paths = [row.source_file for row in df.select("source_file").collect()]
-        write_successes(
-            success_paths, activity.info().workflow_id, activity.info().activity_id
-        )
 
-        activity.logger.info("Finished")
-        return num_hl7
     except (Py4JError, ConnectionError) as e:
         activity.logger.error(
             "Spark error ingesting HL7 files to Delta Lake. Marking pod unhealthy."
@@ -356,21 +401,37 @@ def import_hl7_files_to_deltalake(
         raise
     except TimeoutError:
         activity.logger.info("Temporal activity has been cancelled")
+        return 0
     except Exception as e:
         activity.logger.exception("Error ingesting HL7 files to Delta Lake", exc_info=e)
         raise
     finally:
         if spark is not None:
+            activity.logger.info("Clearing spark cache")
+            activity.heartbeat()
+            try:
+                spark.catalog.clearCache()
+            except Exception as e:
+                activity.logger.error("Error clearing spark cache", exc_info=e)
+
             activity.logger.info("Stopping spark")
+            activity.heartbeat()
             try:
                 spark.stop()
             except:
-                pass
+                activity.logger.error("Error stopping spark", exc_info=e)
 
         if temp_dir is not None:
             # CLean up temp dir after Spark is finished processing
             activity.logger.info("Cleaning up temp dir %s", temp_dir)
+            activity.heartbeat()
             try:
                 shutil.rmtree(temp_dir)
             except Exception as e:
-                activity.logger.error("Error cleaning up temp dir %s", e)
+                activity.logger.error("Error cleaning up temp dir %s", exc_info=e)
+
+    activity.heartbeat()
+    write_successes(success_paths, workflow_id, activity_id)
+
+    activity.logger.info("All done")
+    return len(success_paths)
