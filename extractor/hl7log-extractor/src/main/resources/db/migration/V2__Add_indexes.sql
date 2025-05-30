6f5afb83344d78d4b31db4a73e3a55a9aff68a94
John Flavin <flavinj@gmail.com>
2025-05-30 08:28:14 -0500

    Remove redundant index idx_hl7_files_hl7_file_path

--- extractor/hl7log-extractor/src/main/resources/db/migration/V2__Add_indexes.sql
+++ extractor/hl7log-extractor/src/main/resources/db/migration/V2__Add_indexes.sql
@@ -6,5 +6,4 @@ CREATE INDEX idx_file_statuses_workflow_id ON file_statuses(workflow_id);
 
 -- Indexes for hl7_files
 CREATE INDEX idx_hl7_files_log_file_path ON hl7_files (log_file_path);
-CREATE INDEX idx_hl7_files_hl7_file_path ON hl7_files (hl7_file_path);
 CREATE INDEX idx_hl7_files_date ON hl7_files (date);
