101c9c58a79ee7d17a09147107b1b3c95a0f15fd
John Flavin <flavinj@gmail.com>
2025-06-04 17:17:25 -0500

    javadoc for Hl7LogExtractorApplication

--- extractor/hl7log-extractor/src/main/java/edu/washu/tag/extractor/hl7log/Hl7LogExtractorApplication.java
+++ extractor/hl7log-extractor/src/main/java/edu/washu/tag/extractor/hl7log/Hl7LogExtractorApplication.java
@@ -4,10 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
 
+/**
+ * Main application class for the HL7 Log Extractor.
+ */
 @SpringBootApplication
 @EnableRetry
 public class Hl7LogExtractorApplication {
 
+    /**
+     * Main method to run the HL7 Log Extractor application.
+     *
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         SpringApplication.run(Hl7LogExtractorApplication.class, args);
     }
