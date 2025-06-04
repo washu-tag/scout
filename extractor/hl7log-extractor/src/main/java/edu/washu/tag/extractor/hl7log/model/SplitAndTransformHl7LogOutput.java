ad0cbdf9085e099ebf284301957bd5c6fae84825
John Flavin <flavinj@gmail.com>
2025-06-04 16:10:12 -0500

    Empty space in SplitAndTransformHl7LogInput/Output

--- extractor/hl7log-extractor/src/main/java/edu/washu/tag/extractor/hl7log/model/SplitAndTransformHl7LogOutput.java
+++ extractor/hl7log-extractor/src/main/java/edu/washu/tag/extractor/hl7log/model/SplitAndTransformHl7LogOutput.java
@@ -1,3 +1,3 @@
 package edu.washu.tag.extractor.hl7log.model;
 
-public record SplitAndTransformHl7LogOutput(String hl7FilesOutputFilePath, int numHl7Files) { }
+public record SplitAndTransformHl7LogOutput(String hl7FilesOutputFilePath, int numHl7Files) {}
