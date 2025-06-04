ddbe97d5e1e68f3adc93853ef4ab249c52f52a90
John Flavin <flavinj@gmail.com>
2025-06-04 16:11:03 -0500

    Spaces between methods in Hl7File

--- extractor/hl7log-extractor/src/main/java/edu/washu/tag/extractor/hl7log/db/Hl7File.java
+++ extractor/hl7log-extractor/src/main/java/edu/washu/tag/extractor/hl7log/db/Hl7File.java
@@ -11,6 +11,7 @@ public record Hl7File(
     private static final String UPSERT_ON_CONFLICT_SQL =
          "ON CONFLICT (hl7_file_path) "
             + "DO UPDATE SET log_file_path = EXCLUDED.log_file_path, message_number = EXCLUDED.message_number, date = EXCLUDED.date";
+
     public static String getUpsertSql() {
         return UPSERT_ON_CONFLICT_SQL;
     }
