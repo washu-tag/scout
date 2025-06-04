c810ae49596d4ed8bdda90ed635c03a50be230b2
John Flavin <flavinj@gmail.com>
2025-06-04 17:38:05 -0500

    Unused imports

--- extractor/hl7log-extractor/src/main/java/edu/washu/tag/extractor/hl7log/workflow/RefreshIngestDbViewsWorkflow.java
+++ extractor/hl7log-extractor/src/main/java/edu/washu/tag/extractor/hl7log/workflow/RefreshIngestDbViewsWorkflow.java
@@ -1,7 +1,5 @@
 package edu.washu.tag.extractor.hl7log.workflow;
 
-import edu.washu.tag.extractor.hl7log.model.IngestHl7LogWorkflowInput;
-import edu.washu.tag.extractor.hl7log.model.IngestHl7LogWorkflowOutput;
 import edu.washu.tag.extractor.hl7log.model.RefreshIngestDbViewsInput;
 import edu.washu.tag.extractor.hl7log.model.RefreshIngestDbViewsOutput;
 import io.temporal.workflow.WorkflowInterface;
