672b4415122e20c40d30793ff6ec6ae6d23f669c
Kate Alpert <alpertk@wustl.edu>
2025-11-05 12:32:27 -0700

    Show chat in launchpad

--- launchpad/public/config.js
+++
@@ -1,6 +0,0 @@
-// Default configuration for local development
-// This file will be overridden in production by Kubernetes ConfigMap
-window.scoutConfig = {
-  enableChat: false,
-  showPlaybooks: false
-};
