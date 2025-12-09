1fcb02611bd82ff83fd0009909157b15d6560a81
Charlie Moore <moore.c@wustl.edu>
2025-12-09 16:50:27 -0600

    More maintainable structure and full derivative dependency structure

--- extractor/hl7-transformer/src/hl7scout/hl7extractor/dataextraction.py
+++ extractor/hl7-transformer/src/hl7scout/hl7extractor/dataextraction.py
@@ -1,40 +1,41 @@
-from dataclasses import dataclass
-from typing import Callable, List, Optional
+from dataclasses import dataclass, field
+from typing import Callable, Optional, Dict
 from temporalio import activity
 
-from delta import DeltaTable
 from pyspark.sql import SparkSession, DataFrame, Window
-from pyspark.sql import functions as F
+
+from .curatedtable import curated_table
+from .diagnosistable import diagnosis_table
+from .latesttable import latest_table
 
 
 @dataclass
 class DerivativeTable:
-    name: str
     source_table: str
+    table_name: str
     process_source_data: Callable[[DataFrame, SparkSession, str], None]
+    children_tables: dict[str, "DerivativeTable"] = field(
+        default_factory=dict, init=False
+    )
 
 
 def define_derivative_tables(report_table_name: str):
-    return [
-        DerivativeTable(
-            name=f"{report_table_name}_latest",
-            source_table=report_table_name,
-            process_source_data=process_latest_table,
-        ),
-        DerivativeTable(
-            name=f"{report_table_name}_dx",
-            source_table=f"{report_table_name}_latest",
-            process_source_data=process_dx_denormalize,
-        ),
-    ]
+    return {
+        table.table_name: table
+        for table in [
+            curated_table(report_table_name),
+            latest_table(report_table_name),
+            diagnosis_table(report_table_name),
+        ]
+    }
 
 
-def perform_table_operations(spark: SparkSession, tables: List[DerivativeTable]):
-    source_table = tables[0].source_table
-
+def perform_table_operations(
+    spark: SparkSession, source_table: str, tables: Dict[str, DerivativeTable]
+):
     def streaming_function(batch_df, batch_id):
-        for table in tables:
-            table.process_source_data(batch_df, spark, table.name)
+        for name, table in tables.items():
+            table.process_source_data(batch_df, spark, f"default.{name}")
 
     full_table = (
         spark.readStream.format("delta")
@@ -50,123 +51,27 @@ def perform_table_operations(spark: SparkSession, tables: List[DerivativeTable])
 
     latest_table_operation.awaitTermination()
 
+    activity.heartbeat()
+    activity.logger.info(f"Derivative tables {tables.keys()} updated")
 
-def process_latest_table(batch_df, spark, base_table_name):
-    """
-    Keeps only 1 report per accession number
-    """
-
-    table_name = f"default.{base_table_name}"
-    deduped_df = dedupe_df_on_accession_number(batch_df)
-    if deduped_df is None:
-        return
-
-    # Update existing latest table or create it if it does not yet exist
-    if spark.catalog.tableExists(table_name):
-        latest_table = DeltaTable.forName(spark, table_name)
-        update_set = {
-            col_name: F.col(f"s.{col_name}") for col_name in deduped_df.columns
-        }
-
-        latest_table.alias("t").merge(
-            deduped_df.alias("s"),
-            "t.obr_3_filler_order_number = s.obr_3_filler_order_number",
-        ).whenMatchedUpdate(
-            "s.message_dt > t.message_dt",
-            update_set,  # Use exact columns as in source
-        ).whenNotMatchedInsertAll().execute()  # If no existing row for accession number, insert it as-is
-    else:
-        create_table_from_df(deduped_df, base_table_name)
-
-
-def process_dx_denormalize(batch_df, spark, base_table_name):
-    """
-    Uses latest table
-    """
-    deduped_df = dedupe_df_on_accession_number(batch_df)
-    if deduped_df is None:
-        return
-
-    table_name = f"default.{base_table_name}"
-
-    exploded_df = (
-        deduped_df.withColumn("diagnosis", F.explode("diagnoses"))
-        .withColumn("diagnosis_code", F.col("diagnosis.diagnosis_code"))
-        .withColumn("diagnosis_code_text", F.col("diagnosis.diagnosis_code_text"))
-        .withColumn(
-            "diagnosis_code_coding_system",
-            F.col("diagnosis.diagnosis_code_coding_system"),
-        )
-        .drop("diagnosis", "diagnoses", "diagnoses_consolidated")
-    )
-
-    # For accession numbers we already have, delete previous diagnoses to replace them
-    if spark.catalog.tableExists(table_name):
-        accession_numbers = exploded_df.select("obr_3_filler_order_number").distinct()
-        existing_table = DeltaTable.forName(spark, table_name)
-
-        existing_table.alias("t").merge(
-            accession_numbers.alias("s"),
-            "t.obr_3_filler_order_number = s.obr_3_filler_order_number",
-        ).whenMatchedDelete().execute()
-
-    create_table_from_df(exploded_df, base_table_name)
+    for child_name, child_table in tables.items():
+        perform_table_operations(spark, child_name, child_table.children_tables)
 
 
 def process_derivative_data(spark: SparkSession, report_table_name: str):
     derivative_tables = define_derivative_tables(report_table_name)
     activity.heartbeat()
     activity.logger.info(
-        f"Processing derivative data tables: {[table.name for table in derivative_tables]}"
+        f"Processing derivative data tables: {[table.name for name, table in derivative_tables]}"
     )
-    tables_without_dependencies = []
-    tables_with_dependencies = []
-    for table in derivative_tables:
-        (
-            tables_without_dependencies
-            if table.source_table == report_table_name
-            else tables_with_dependencies
-        ).append(table)
-    perform_table_operations(spark, tables_without_dependencies)
-    activity.heartbeat()
-    activity.logger.info(
-        f"Derivative tables without dependencies updated: {[table.name for table in tables_without_dependencies]}"
+    # noinspection PyTypeChecker
+    root_table = DerivativeTable(
+        source_table=None,
+        table_name=report_table_name,
+        process_source_data=None,
     )
-    for table in tables_with_dependencies:
-        # TODO: this is currently lazy just to make a PoC. This (and realistically the preceding part) should all just resolve the derivative tables as a DAG
-        perform_table_operations(spark, [table])
-        activity.heartbeat()
-        activity.logger.info(f"Derivative table {table.name} updated")
-
-
-def dedupe_df_on_accession_number(batch_df: DataFrame) -> Optional[DataFrame]:
-    if batch_df.isEmpty():
-        return None
-
-    updates_insert_df = batch_df.filter(
-        F.col("_change_type").isin(["insert", "update_postimage"])
-    ).drop("_change_type", "_commit_version", "_commit_timestamp")
-
-    if updates_insert_df.isEmpty():
-        return None
-
-    # First, make sure our new data only has the newest report per accession number
-    dedupe_window = Window.partitionBy("obr_3_filler_order_number").orderBy(
-        F.desc("message_dt")
-    )
-    # We have to create an explicit column instead of filtering by window function
-    # even though it looks tempting to put it all in the filter
-    return (
-        updates_insert_df.withColumn("report_index", F.row_number().over(dedupe_window))
-        .filter(F.col("report_index") == 1)
-        .drop("report_index")
-    )
-
-
-def create_table_from_df(df: DataFrame, table_name: str):
-    (
-        df.write.format("delta")
-        .option("delta.enableChangeDataFeed", "true")
-        .mode("append")
-        .saveAsTable(f"default.{table_name}")
-    )
+    derivative_tables[report_table_name] = root_table
+    for name, table in derivative_tables.items():
+        if table.source_table is not None:
+            derivative_tables[table.source_table].children_tables[name] = table
+    perform_table_operations(spark, report_table_name, root_table.children_tables)
