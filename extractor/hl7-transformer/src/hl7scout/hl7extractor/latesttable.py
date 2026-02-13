8fd558e663bc7d22ab5c3a526c80d90bfa1f167f
Charlie Moore <cmoore01@users.noreply.github.com>
2026-02-13 11:40:37 -0600

    Derivative Table Infrastructure and implementations (#282)
    Derivative Table Infrastructure and implementations (#282)

---
+++ extractor/hl7-transformer/src/hl7scout/hl7extractor/latesttable.py
@@ -0,0 +1,39 @@
+from delta import DeltaTable
+from .derivativetable import DerivativeTable
+from .sparkutils import dedupe_df_on_accession_number, create_table_from_df
+from pyspark.sql import functions as F
+
+
+def latest_table(base_report_table_name: str) -> DerivativeTable:
+    return DerivativeTable(
+        source_table=f"{base_report_table_name}_curated",
+        table_name=f"{base_report_table_name}_latest",
+        process_source_data=process_latest_table,
+    )
+
+
+def process_latest_table(batch_df, spark, table_name):
+    """
+    Keeps only 1 report per accession number
+    """
+
+    deduped_df = dedupe_df_on_accession_number(batch_df)
+    if deduped_df is None:
+        return
+
+    # Update existing latest table or create it if it does not yet exist
+    if spark.catalog.tableExists(table_name):
+        latest_dt = DeltaTable.forName(spark, table_name)
+        update_set = {
+            col_name: F.col(f"s.{col_name}") for col_name in deduped_df.columns
+        }
+
+        latest_dt.alias("t").merge(
+            deduped_df.alias("s"),
+            "t.accession_number = s.accession_number",
+        ).whenMatchedUpdate(
+            "s.message_dt > t.message_dt",
+            update_set,  # Use exact columns as in source
+        ).whenNotMatchedInsertAll().execute()  # If no existing row for accession number, insert it as-is
+    else:
+        create_table_from_df(deduped_df, table_name)
