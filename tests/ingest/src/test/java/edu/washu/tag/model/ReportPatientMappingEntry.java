c763e81f7c61a8b21bb01b90677c774d7b253c7c
Charlie Moore <moore.c@wustl.edu>
2026-04-03 09:40:07 -0500

    Switched mapping table to not be global. Added tests

---
+++ tests/ingest/src/test/java/edu/washu/tag/model/ReportPatientMappingEntry.java
@@ -0,0 +1,55 @@
+package edu.washu.tag.model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class ReportPatientMappingEntry {
+
+    private String scoutPatientId;
+    private String primaryReportIdentifier;
+    private String mpi;
+    private String epicMrn;
+    private String consistent;
+
+    public String getScoutPatientId() {
+        return scoutPatientId;
+    }
+
+    public void setScoutPatientId(String scoutPatientId) {
+        this.scoutPatientId = scoutPatientId;
+    }
+
+    public String getPrimaryReportIdentifier() {
+        return primaryReportIdentifier;
+    }
+
+    public void setPrimaryReportIdentifier(String primaryReportIdentifier) {
+        this.primaryReportIdentifier = primaryReportIdentifier;
+    }
+
+    public String getMpi() {
+        return mpi;
+    }
+
+    public void setMpi(String mpi) {
+        this.mpi = mpi;
+    }
+
+    public String getEpicMrn() {
+        return epicMrn;
+    }
+
+    public void setEpicMrn(String epicMrn) {
+        this.epicMrn = epicMrn;
+    }
+
+    public String getConsistent() {
+        return consistent;
+    }
+
+    public void setConsistent(String consistent) {
+        this.consistent = consistent;
+    }
+
+}
