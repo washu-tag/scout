ea390c1409755dec8ec3154637b49d175933f594
Charlie Moore <moore.c@wustl.edu>
2026-05-12 12:24:47 -0500

    Removed variable declaration in mapping table

--- extractor/hl7-transformer/src/hl7scout/hl7extractor/mappingtable.py
+++ extractor/hl7-transformer/src/hl7scout/hl7extractor/mappingtable.py
@@ -3,13 +3,11 @@ from .mappingtableextractor import MappingTableExtractor
 
 
 def mapping_table(base_report_table_name: str) -> DerivativeTable:
-    source_table = f"{base_report_table_name}_curated"
-
     def process_table(batch_df, spark, table_name):
         MappingTableExtractor(spark, table_name).extract(batch_df)
 
     return DerivativeTable(
-        source_table=source_table,
+        source_table=f"{base_report_table_name}_curated",
         table_name=f"{base_report_table_name}_report_patient_mapping",
         process_source_data=process_table,
     )
